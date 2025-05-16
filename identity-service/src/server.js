7cdc977d37979331b57eba2b3cf61b8cd954abc1
aleksandar.benefiti <aleksandar@benefiti.rs>
2025-05-16 14:35:09 +0200

    Handle bootstraping identity service with mongoose connection.

--- identity-service/src/server.js
+++ identity-service/src/server.js
@@ -22,10 +22,18 @@ app.use(express.json());
 
 app.use("/auth", authRoutes);
 
-app.listen(ENV_VARIABLES.PORT, async () => {
-	await mongoose.connect(ENV_VARIABLES.MONGO_DB_URI);
-	logger.info(`Identity service running on port ${ENV_VARIABLES.PORT}`);
-});
+async function bootstrap() {
+	try {
+		await mongoose.connect(ENV_VARIABLES.MONGO_DB_URI);
+		logger.info("Identity service is connected to DB");
+
+		app.listen(ENV_VARIABLES.PORT, async () => {
+			logger.info(`Identity service running on port ${ENV_VARIABLES.PORT}`);
+		});
+	} catch (error) {
+		logger.error(error);
+	}
+}
 
 process.on("unhandledRejection", (reason, promise) => {
 	logger.error("Unhandled Rejection at", promise, "reason:", reason);
@@ -34,3 +42,5 @@ process.on("unhandledRejection", (reason, promise) => {
 process.on("uncaughtException", (reason, promise) => {
 	logger.error("Uncaught Exception at", promise, "reason:", reason);
 });
+
+bootstrap();
